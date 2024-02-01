3e8ad6871817fc2a0643b9acdefdaa5031699f3e
SkilzHax <143220530+SkilzHax@users.noreply.github.com>
2024-01-31 19:29:52 -0800

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -37,37 +37,23 @@ const initialCards = [
 ];
 
 const profileEditBtn = document.querySelector("#edit-cursor-pointer");
-
 const profileAddBtn = document.querySelector("#add-cursor-pointer");
-
 const profileEditModal = document.querySelector("#edit-modal");
-
 const cardAddModal = document.querySelector("#add-card-modal");
 
 const previewImg = document.querySelector("#image__modal");
-
 const profileEditCls = profileEditModal.querySelector("#edit-close");
-
 const cardClose = cardAddModal.querySelector("#card-edit-close");
-
 const imgCls = previewImg.querySelector("#image-edit-close");
-
 const profileName = document.querySelector(".profile__name");
-
 const profileJob = document.querySelector(".profile__job");
-
 const addCardElement = cardAddModal.querySelector(".modal__form");
 
 const imgPopout = previewImg.querySelector(".modal-pop-out");
-
 const imgPopoutDesc = previewImg.querySelector(".modal__description");
-
 const profileNameInput = document.querySelector("#profile-name-input");
-
 const profileJobInput = document.querySelector("#profile-job-input");
-
 const cardTitleInput = addCardElement.querySelector(".modal__name");
-
 const cardImgInput = addCardElement.querySelector(".modal__URL");
 
 const profileEditForm = profileEditModal.querySelector(".modal__form");
@@ -95,7 +81,6 @@ function handleAddCardFormSubmit(evt) {
   evt.preventDefault();
 
   const name = cardTitleInput.value;
-
   const link = cardImgInput.value;
 
   renderCard({ name, link }, cardListEL);
@@ -105,13 +90,9 @@ function handleAddCardFormSubmit(evt) {
 
 function getCardElement(cardData) {
   const cardElement = cardTemplate.cloneNode(true);
-
   const cardImageEL = cardElement.querySelector(".card__image");
-
   const cardTitleEL = cardElement.querySelector(".card__description");
-
   const likeBtn = cardElement.querySelector(".card__like-button");
-
   const trashBtn = cardElement.querySelector(".trash__icon");
 
   trashBtn.addEventListener("click", () => {
@@ -120,7 +101,7 @@ function getCardElement(cardData) {
 
   cardImageEL.addEventListener("click", () => {
     imgPopout.src = cardImageEL.src;
-
+    imgPopoutDesc.alt = cardTitleEL.innerText;
     imgPopoutDesc.textContent = cardTitleEL.innerText;
 
     openModal(previewImg);
@@ -133,9 +114,7 @@ function getCardElement(cardData) {
   });
 
   cardTitleEL.textContent = cardData.name;
-
   cardImageEL.src = cardData.link;
-
   cardTitleEL.alt = cardData.name;
 
   return cardElement;
@@ -145,21 +124,17 @@ profileEditBtn.addEventListener("click", () => {
   openModal(profileEditModal);
 
   profileNameInput.value = profileName.textContent;
-
   profileJobInput.value = profileJob.textContent;
 });
 
 profileAddBtn.addEventListener("click", () => openModal(cardAddModal));
-
 profileEditCls.addEventListener("click", () => closePopup(profileEditModal));
-
 cardClose.addEventListener("click", () => closePopup(cardAddModal));
 
 profileEditForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   profileName.textContent = profileNameInput.value;
-
   profileJob.textContent = profileJobInput.value;
 
   closePopup(profileEditModal);
