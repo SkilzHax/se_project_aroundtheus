68e570292206a60a9f42318388881e79ff6eb8c1
SkilzHax <143220530+SkilzHax@users.noreply.github.com>
2024-02-02 02:51:59 -0800

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,31 @@
 const initialCards = [
   {
     name: "Yosemite Valley",
-
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
   },
 
   {
     name: "Lake Louise",
-
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
   },
 
   {
     name: "Bald Mountains",
-
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
   },
 
   {
     name: "Latemar",
-
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
   },
 
   {
     name: "Vanoise National Park",
-
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
   },
 
   {
     name: "Lago di Braies",
-
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
@@ -51,7 +45,7 @@ const addCardElement = cardAddModal.querySelector(".modal__form");
 
 const imgPopout = previewImg.querySelector(".modal-pop-out");
 const imgPopoutDesc = previewImg.querySelector(".modal__description");
-const imgAlt = previewImg.querySelector(".imgalt");
+const imgAlt = previewImg.querySelector("#imgalt");
 const profileNameInput = document.querySelector("#profile-name-input");
 const profileJobInput = document.querySelector("#profile-job-input");
 const cardTitleInput = addCardElement.querySelector(".modal__name");
